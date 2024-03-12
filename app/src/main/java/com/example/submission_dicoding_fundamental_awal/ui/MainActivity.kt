3957c7d57450143801ae320e53dcfe19dc06b068
Afdholanabil <afdholanabil41435@gmail.com>
2024-03-12 10:23:50 +0700

    adding color theme, package response Gson, retrofit ApiConf,ApiService & design activity_main

--- app/src/main/java/com/example/submission_dicoding_fundamental_awal/ui/MainActivity.kt
+++ app/src/main/java/com/example/submission_dicoding_fundamental_awal/ui/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.submission_dicoding_fundamental_awal.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.submission_dicoding_fundamental_awal.R
+import com.example.submission_dicoding_fundamental_awal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            searchView.setupWithSearchBar(searchBar)
+            searchView.editText.setOnEditorActionListener { textView, actionId, event ->
+                searchBar.hint = searchView.text
+                searchView.hide()
+                Toast.makeText(this@MainActivity, searchView.text, Toast.LENGTH_SHORT).show()
+                false
+            }
+        }
+
+
     }
 }

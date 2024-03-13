eda052e498708e718bd305a1e4190edcef3d4fb2
Afdholanabil <afdholanabil41435@gmail.com>
2024-03-13 11:20:53 +0700

    adding DetailUserViewModel

--- app/src/main/java/com/example/submission_dicoding_fundamental_awal/ui/DetailUserActivity.kt
+++ app/src/main/java/com/example/submission_dicoding_fundamental_awal/ui/DetailUserActivity.kt
@@ -3,8 +3,10 @@ package com.example.submission_dicoding_fundamental_awal.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.submission_dicoding_fundamental_awal.R
+import com.example.submission_dicoding_fundamental_awal.databinding.ActivityDetailUserBinding
 
 class DetailUserActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailUserBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_user)
---
+++ app/src/main/java/com/example/submission_dicoding_fundamental_awal/ui/DetailUserViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.submission_dicoding_fundamental_awal.ui
+
+import androidx.lifecycle.ViewModel
+
+class DetailUserViewModel : ViewModel() {
+}

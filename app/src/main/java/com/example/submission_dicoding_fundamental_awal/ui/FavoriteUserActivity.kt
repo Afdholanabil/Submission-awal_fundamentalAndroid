46348697a3c80dbe87a50783b4b2780746882b72
Afdholanabil <afdholanabil41435@gmail.com>
2024-03-19 20:48:17 +0700

    adding FavoriteUserActivity

---
+++ app/src/main/java/com/example/submission_dicoding_fundamental_awal/ui/FavoriteUserActivity.kt
@@ -0,0 +1,12 @@
+package com.example.submission_dicoding_fundamental_awal.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.submission_dicoding_fundamental_awal.R
+
+class FavoriteUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite_user)
+    }
+}
